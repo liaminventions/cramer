fd8f3c8fb7ae13a7d55ec9b6968a4ca1821fd5a2
Waverider <unstinkable919@gmail.com>
2024-07-06 18:43:20 -0400

    can print matricies now :3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
 package org.example
 
 var matrix = arrayOf(intArrayOf(2, 3), intArrayOf(5, 2))
+
+fun printMatrix(mat: Array<IntArray>) {
+    val cols: Int = mat[0].size
+    val rows: Int = mat[1].size
+    for (i: Int in 0..<rows) {
+        for (j: Int in 0..<cols) {
+            print("${mat[i][j]}\t")
+        }
+        print("\n")
+    }
+    print("\n")
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun det2(mat: Array<IntArray>): Int {
@@ -9,4 +21,5 @@ fun det2(mat: Array<IntArray>): Int {
 
 fun main() {
     println(det2(matrix))
+    printMatrix(matrix)
 }
